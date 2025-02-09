b52d6b564190627cec857c813a29a1975068b089
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-02-09 16:06:58 -0300

    `feat`: manages Controllers to handle GET requests

--- src/main/java/com/brunotadashi/neospring/web/NeoDispatchServlet.java
+++ src/main/java/com/brunotadashi/neospring/web/NeoDispatchServlet.java
@@ -1,5 +1,9 @@
 package com.brunotadashi.neospring.web;
 
+import com.brunotadashi.neospring.datastructures.ControllersInstances;
+import com.brunotadashi.neospring.datastructures.ControllersMap;
+import com.brunotadashi.neospring.datastructures.RequestControllerData;
+import com.brunotadashi.neospring.util.NeoLogger;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 
 public class NeoDispatchServlet extends HttpServlet {
 
@@ -19,11 +24,37 @@ public class NeoDispatchServlet extends HttpServlet {
             return;
         }
 
-        System.out.println("URL = " + request.getRequestURL().toString());
-        System.out.println("METHOD = " + request.getMethod());
+        String url = request.getRequestURI();
+        String httpMethod = request.getMethod().toUpperCase();
+        String key = httpMethod + url;
+        RequestControllerData data = ControllersMap.values.get(key);
+        NeoLogger.log("NeoDispatcherServlet", "URL: " + url + " (" + httpMethod + ") - Handler " + data.getControllerClass() + "." + data.getControllerMethod());
 
-        PrintWriter out = new PrintWriter(response.getWriter());
-        out.println("Hello World from NeoSpring");
-        out.close();
+        Object controller;
+        NeoLogger.log("DispatcherServlet", "Searching for Controller Instance");
+        try {
+            controller = ControllersInstances.instances.get(data.controllerClass);
+            if (controller == null) {
+                NeoLogger.log("DispatcherServlet", "Creating new Controller Instance");
+                controller = Class.forName(data.controllerClass).getDeclaredConstructor().newInstance();
+                ControllersInstances.instances.put(data.controllerClass, controller);
+            }
+
+            Method controllerMethod = null;
+            for (Method method : controller.getClass().getMethods()) {
+                if (method.getName().equals(data.controllerMethod)) {
+                    controllerMethod = method;
+                    break;
+                }
+            }
+
+            NeoLogger.log("DispatcherServlet", "Invoking method " + controllerMethod.getName() + " to handle request");
+            PrintWriter out = new PrintWriter(response.getWriter());
+            // Invoca o método a partir da instância desse Controller
+            out.println(controllerMethod.invoke(controller));
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
