0336c1e35914a7667c3298125496790ed10cbb03
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-03-02 15:29:41 -0300

    `feat`: handles POST request with a request body parameter

--- src/main/java/com/brunotadashi/neospring/web/NeoDispatchServlet.java
+++ src/main/java/com/brunotadashi/neospring/web/NeoDispatchServlet.java
@@ -10,9 +10,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class NeoDispatchServlet extends HttpServlet {
 
@@ -27,7 +30,6 @@ public class NeoDispatchServlet extends HttpServlet {
 
         PrintWriter out = new PrintWriter(response.getWriter());
         Gson gson = new Gson();
-
 
         String url = request.getRequestURI();
         String httpMethod = request.getMethod().toUpperCase();
@@ -54,10 +56,36 @@ public class NeoDispatchServlet extends HttpServlet {
             }
 
             NeoLogger.log("DispatcherServlet", "Invoking method " + controllerMethod.getName() + " to handle request");
-            out.println(gson.toJson(controllerMethod.invoke(controller)));
+
+            if (controllerMethod.getParameterCount() > 0) {
+                NeoLogger.log("NeoDispatchServlet", "Method " + controllerMethod.getName() + " has parameters");
+                Object arg;
+                Parameter parameter = controllerMethod.getParameters()[0];
+                if (parameter.getAnnotations()[0].annotationType().getName().equals("com.brunotadashi.neospring.annotations.NeoBody")) {
+                    String body = readBytesFromRequest(request);
+                    NeoLogger.log("", "    Found Parameter from request of type " + parameter.getType().getName());
+                    NeoLogger.log("", "    Parameter content: " + body);
+                    arg = gson.fromJson(body, parameter.getType());
+                    out.println(gson.toJson(controllerMethod.invoke(controller, arg)));
+                }
+            } else {
+                out.println(gson.toJson(controllerMethod.invoke(controller)));
+            }
+
             out.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private String readBytesFromRequest(HttpServletRequest request) throws Exception {
+        StringBuilder str = new StringBuilder();
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        while ((line = br.readLine()) != null) {
+            str.append(line);
+        }
+
+        return str.toString();
     }
 }
